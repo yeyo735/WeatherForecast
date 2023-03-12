a587c6eca2091c485464c97dff5bf0e1301bdf32
Sergio Miranda <yeyo735@gmail.com>
2023-03-12 19:34:14 -0400

    added all data to view

--- app/src/main/java/com/yeyosystem/weatherforecast/FirstFragment.kt
+++ app/src/main/java/com/yeyosystem/weatherforecast/FirstFragment.kt
@@ -1,16 +1,23 @@
 package com.yeyosystem.weatherforecast
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.Lifecycle
+import com.bumptech.glide.Glide
+import com.yeyosystem.weatherforecast.data.Current
+import com.yeyosystem.weatherforecast.data.ForecastDay
+import com.yeyosystem.weatherforecast.data.Location
 import com.yeyosystem.weatherforecast.databinding.FragmentFirstBinding
 import com.yeyosystem.weatherforecast.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -38,17 +45,67 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        weatherViewModel.weathers.observe(viewLifecycleOwner){
-            it
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.menu_main, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                return when (menuItem.itemId) {
+                    R.id.search -> {
+                        // clearCompletedTasks()
+                        true
+                    }
+                    /*R.id.menu_refresh -> {
+                        // loadTasks(true)
+                        true
+                    }*/
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        showCurrentHour()
+        weatherViewModel.weathers.observe(viewLifecycleOwner) {
+            showLocationInformation(it.location)
+            showCurrentInformation(it.current)
+            showNextDays(it.forecast.forecastDay)
         }
         binding.viewModel = weatherViewModel
-        /*weatherViewModel.weathers.observe(viewLifecycleOwner){
-            it
-        }*/
+    }
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+    private fun showNextDays(forecastDay: List<ForecastDay>) {
+
+    }
+
+    private fun showCurrentHour() {
+        val sdf = SimpleDateFormat("hh:mm a", Locale.getDefault())
+        val currentTime: String = sdf.format(Date())
+        binding.textHour.text = currentTime
+    }
+
+    private fun showLocationInformation(location: Location) {
+        binding.textCity.text = location.name
+        binding.textDate.text = weatherViewModel.formatDateCurrent(location.localtime)
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun showCurrentInformation(current: Current) {
+        binding.textCurrentTemperature.text =
+            getString(R.string.temperature, current.temp_f.toString())
+        binding.textCurrentWind.text = getString(R.string.wind, current.wind_mph.toInt().toString())
+        binding.textCurrentPressure.text = getString(R.string.press, current.precip_in.toInt().toString())
+        binding.textCurrentCondition.text =
+            getString(R.string.condition, current.condition.text.lowercase())
+        Glide
+            .with(this@FirstFragment)
+            .load(current.condition.icon.replace("//", "https://"))
+            .centerCrop()
+            .into(binding.imageCurrent)
     }
 
     override fun onDestroyView() {
